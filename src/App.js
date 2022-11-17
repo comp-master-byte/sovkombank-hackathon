95335088c4a1a8c2daf5c3e9a9f22dee55a544db
comp-master-byte <demoniac781@gmail.com>
2022-11-17 22:47:10 +0500

    Сборка проекта

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Theme, presetGpnDefault } from '@consta/uikit/Theme';
+import { Button } from '@consta/uikit/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Theme preset={presetGpnDefault}>
+            <Button label="Кнопка" />
+        </Theme>      
+    );
 }
 
 export default App;
